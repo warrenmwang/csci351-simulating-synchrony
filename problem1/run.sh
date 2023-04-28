90c486d9a5700548c0cb4554c96db9f5ca4c655b
Warren Wang <warren.wang2020@gmail.com>
2023-04-28 09:21:16 -0400

    small changes, another test in problem1/2, still need to write pseudocode and analysis

--- problem1/run.sh
+++ problem1/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Running with 4 processes..."
 mpiexec -n 4 python main.py --n 4 > output.txt
+python showResults.py output.txt
+rm output.txt
+
+echo ""
+echo "Running with 40 processes..."
+mpiexec -n 40 python main.py --n 40 > output.txt
 python showResults.py output.txt
 rm output.txt
